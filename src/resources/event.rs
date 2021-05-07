522563d150b5c02a53f4fb40375dd2556c8027fa
mikeumus <mikeumus@gmail.com>
2021-05-06 23:56:28 -0400

    adding println! to see issue

--- src/resources/event.rs
+++ src/resources/event.rs
@@ -193,6 +193,7 @@ pub struct Event {
     #[serde(rename = "type")]
     pub event_type: EventType,
     pub data: EventData,
+    pub livemode: bool,
     // ...
 }
 
@@ -254,6 +255,13 @@ impl Webhook {
         let signature = Signature::parse(&sig)?;
         let signed_payload = format!("{}{}{}", signature.t, ".", payload);
 
+        let event: Event = serde_json::from_str(&payload).map_err(WebhookError::BadParse)?;
+
+        let sign = if event.livemode {
+            signature.v1
+        } else {
+            signature.v0.ok_or(WebhookError::MissingTestmodeSignature)?
+        };
         // Compute HMAC with the SHA256 hash function, using endpoing secret as key
         // and signed_payload string as the message.
         let mut mac =
@@ -261,16 +269,47 @@ impl Webhook {
         mac.input(signed_payload.as_bytes());
         let mac_result = mac.result();
         let hex = to_hex(mac_result.code().as_slice());
-        if hex != signature.v1 {
+        // if hex != signature.v1 {
+        if hex != sign {
+        // if !mac.result().is_equal(sign.as_bytes()) {
+            println!("  👿 hex != sign");
             return Err(WebhookError::BadSignature);
         }
 
         // Get current timestamp to compare to signature timestamp
         if (self.current_timestamp - signature.t).abs() > 300 {
+        // let current = Utc::now().timestamp();
+        // if current - signature.t > 300 {
             return Err(WebhookError::BadTimestamp(signature.t));
         }
 
-        serde_json::from_str(&payload).map_err(WebhookError::BadParse)
+        Ok(event)
+    }
+}
+
+#[cfg(feature = "webhooks")]
+#[derive(Debug)]
+struct Signature<'r> {
+    t: i64,
+    v1: &'r str,
+    v0: Option<&'r str>,
+}
+
+#[cfg(feature = "webhooks")]
+impl<'r> Signature<'r> {
+    fn parse(raw: &'r str) -> Result<Signature<'r>, WebhookError> {
+        let mut headers = raw.split(',');
+        println!("  👿 timestamp_header = headers.next().ok_or");
+        let timestamp_header = headers.next().ok_or(WebhookError::BadSignature)?;
+        println!("  👿 v1_header = headers.next().ok_or");
+        let v1_header = headers.next().ok_or(WebhookError::BadSignature)?;
+        let v0_header = headers.next();
+        println!("  👿 timestamp_header.split('=').skip(1)");
+        let t = timestamp_header.split('=').skip(1).next().ok_or(WebhookError::BadSignature)?;
+        println!("  👿 v1_header.split('=').skip(1)");
+        let v1 = v1_header.split('=').skip(1).next().ok_or(WebhookError::BadSignature)?;
+        let v0 = v0_header.and_then(|header| header.split('=').skip(1).next());
+        Ok(Signature { t: t.parse::<i64>().map_err(WebhookError::BadHeader)?, v1, v0 })
     }
 }
 
@@ -321,6 +360,37 @@ impl<'r> Signature<'r> {
 
 #[cfg(test)]
 mod tests {
+    #[cfg(feature = "webhooks")]
+    #[test]
+    fn test_signature_parse() {
+        use super::Signature;
+
+        let raw_signature =
+            "t=1492774577,v1=5257a869e7ecebeda32affa62cdca3fa51cad7e77a0e56ff536d0ce8e108d8bd";
+        let signature = Signature::parse(raw_signature).unwrap();
+        assert_eq!(signature.t, 1492774577);
+        assert_eq!(
+            signature.v1,
+            "5257a869e7ecebeda32affa62cdca3fa51cad7e77a0e56ff536d0ce8e108d8bd"
+        );
+        assert_eq!(signature.v0, None);
+
+        let raw_signature_with_test_mode = "t=1492774577,v1=5257a869e7ecebeda32affa62cdca3fa51cad7e77a0e56ff536d0ce8e108d8bd,v0=6ffbb59b2300aae63f272406069a9788598b792a944a07aba816edb039989a39";
+        let signature = Signature::parse(raw_signature_with_test_mode).unwrap();
+        assert_eq!(signature.t, 1492774577);
+        assert_eq!(
+            signature.v1,
+            "5257a869e7ecebeda32affa62cdca3fa51cad7e77a0e56ff536d0ce8e108d8bd"
+        );
+        assert_eq!(
+            signature.v0,
+            Some("6ffbb59b2300aae63f272406069a9788598b792a944a07aba816edb039989a39")
+        );
+    }
+// }
+
+// #[cfg(test)]
+// mod tests {
     #[cfg(feature = "webhook-events")]
     #[test]
     fn test_signature_parse() {
